60fbf5f161935478b61a3e1016f24f288ec342aa
Michael Landry <mlandryviolin@gmail.com>
2018-11-02 09:36:40 -0400

    got full image

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import HomepageWrapper from './HomepageWrapper'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -28,17 +28,18 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <HomepageWrapper>
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+        </HomepageWrapper>
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,20 @@
 import React from 'react'
-import styled from 'styled-components'
-// import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import CTAButton from '../components/CtaButton'
-import backgroundImage from '../images/background-image.jpg'
-
-const HomepageWrapper = styled.div`
-  background-image: url(${backgroundImage});
-`
 
 const elevatorSpeech = 'I am a software engineer in the Atlanta area. I love building websites using React and Node as well as making things with Java, Python and functional languages . I am a big fan of all things blockchain and AI.'
 const cta = "If you think you might have a project or if you need my help with anything then let's get in touch."
-// const style = {
-//   backgroundImage,
-// }
 
-// `background-image: ${backgroundImage}`
-
-// console.log('style', style)
 
 const IndexPage = () => (
-  <HomepageWrapper>
-    <Layout>
-      <h1>Hi, I'm Michael Landry</h1>
-      <h2>A software engineer.</h2>
-      <h3>It's nice to meet you</h3>
-      <p>{elevatorSpeech}</p>
-      <p>{cta}</p>
-      <CTAButton />
-
-    </Layout>
-  </HomepageWrapper>
+  <Layout>
+    <h1>Hi, I'm Michael Landry</h1>
+    <h2>A software engineer.</h2>
+    <h3>It's nice to meet you</h3>
+    <p>{elevatorSpeech}</p>
+    <p>{cta}</p>
+    <CTAButton />
+  </Layout>
 )
 
 export default IndexPage
