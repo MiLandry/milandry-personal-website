e52d7411eade9f3c86e88af2b70821c5f31f0987
Michael Landry <mlandryviolin@gmail.com>
2018-11-02 13:03:33 -0400

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import CTAButton from '../components/CtaButton'
 const elevatorSpeech = 'I am a software engineer in the Atlanta area. I love building websites using React and Node as well as making things with Java, Python and functional languages . I am a big fan of all things blockchain and AI.'
 const cta = "If you think you might have a project or if you need my help with anything then let's get in touch."
 
-
-// <h3>It's nice to meet you</h3>
 const IndexPage = () => (
   <Layout>
     <h2>Michael Landry</h2>
