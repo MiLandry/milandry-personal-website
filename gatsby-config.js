0dab6eeb4b81cbe32f9031ee96ecddfd540a55da
Michael Landry <mlandryviolin@gmail.com>
2018-11-01 20:55:10 -0400

    image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Michael Landry personal website',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import CTAButton from '../components/CtaButton'
+import backgroundImage from '../images/background-image.jpg'
 
 const elevatorSpeech = 'I am a software engineer in the Atlanta area. I love building websites using React and Node as well as making things with Java, Python and functional languages . I am a big fan of all things blockchain and AI.'
 const cta = "If you think you might have a project or if you need my help with anything then let's get in touch."
@@ -12,6 +13,7 @@ const IndexPage = () => (
     <h2>A software engineer.</h2>
     <h3>It's nice to meet you</h3>
     <p>{elevatorSpeech}</p>
+    <img src={backgroundImage} alt="" />
     <p>{cta}</p>
     <CTAButton />
 
