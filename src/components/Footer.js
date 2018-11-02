14909e7d79fadc247b3a130d1cff5d865e30e179
Michael Landry <mlandryviolin@gmail.com>
2018-11-02 17:17:40 -0400

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import CTAButton from './CtaButton'
+
+const urlForResume = 'https://www.dropbox.com/s/95y94uz2hhvh1xg/F%202018%20resume%20Landry%20%282%29.docx?dl=0'
+const urlForCodeSample = 'https://github.com/MiLandry/milandry-personal-website'
+const onResumeClick = () => {
+  if (typeof window !== 'undefined') {
+    window.open(urlForResume)
+  }
+}
+
+const onCodeSampleClick = () => {
+  if (typeof window !== 'undefined') {
+    window.open(urlForCodeSample)
+  }
+}
+
+const Footer = () => (
+  <div>
+    <CTAButton
+      onClick={onResumeClick}
+    >
+    Resume
+    </CTAButton>
+    <CTAButton
+      onClick={onCodeSampleClick}
+    >
+    Code Sample
+    </CTAButton>
+  </div>
+)
+
+export default Footer
