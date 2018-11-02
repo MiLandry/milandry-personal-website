cb75c9f79712f101f06916d17b749100689e2b39
Michael Landry <mlandryviolin@gmail.com>
2018-11-02 13:05:16 -0400

    clean

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
-import HomepageWrapper from './HomepageWrapper'
 import './layout.css'
 
 const Layout = ({ children }) => (
